84b07a2c7b90ecfb3ab20fdb86e04edd7852643e
Sp1rit <126536582+SFIRegret19@users.noreply.github.com>
2024-11-03 01:37:22 +0700

    Update Потоки на классах.py

--- Потоки на классах.py
+++ Потоки на классах.py
@@ -15,12 +15,14 @@ class Knight(threading.Thread):
         while self.__enemies > 0:
             self.__enemies -= self.power
             day += 1
+            if self.__enemies <= 0:
+                self.__enemies = 0
             print(f'{self.name} сражается {day} день(дня), осталось {self.__enemies} воинов.')
             time.sleep(1)
-        if self.__enemies == 0:
+        if self.__enemies <= 0:
             print(f'{self.name} одержал победу спустя {day} дней(дня)!')
 
-first_knight = Knight('Sir Lancelot', 10)
+first_knight = Knight('Sir Lancelot', 243)
 second_knight = Knight("Sir Galahad", 20)
 
 first_knight.start()
